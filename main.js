14829d77f0e832c022b05bf7d91df31a8aa04625
misanorot <audi16v@gmx.de>
2019-08-13 22:24:13 +0200

    ...

--- main.js
+++ main.js
@@ -52,8 +52,7 @@ class Alarm extends utils.Adapter {
      */
     async onReady() {
         // Initialize your adapter here
-        adapter = this;
-         await main();
+        this.main();
         /*
         For every state in the system there has to be also an object of type state
         Here a simple template for a boolean variable named "testVariable"
