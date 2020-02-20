490db10a97865b62cd1a28632bdd80cdaf4c9a8c
misanorot <audi16v@gmx.de>
2020-02-20 19:09:37 +0100

    add logging

--- main.js
+++ main.js
@@ -13,7 +13,8 @@ const T = require('./lib/Logs.js');
  */
 let adapter;
 const L = T.Translate['de'];
-let clean_ids = [];
+let clean_ids = [],
+    log_list = [];
 const alarm = [],
     warning = [],
     night = [];
@@ -33,7 +34,8 @@ let log,
     shorts;
 
 let schedule_from,
-    schedule_to;
+    schedule_to,
+    schedule_reset;
 /**
  * Starts the adapter instance
  * @param {Partial<ioBroker.AdapterOptions>} [options]
@@ -53,6 +55,7 @@ function startAdapter(options) {
                 adapter.log.info('cleaned everything up...');
                 schedule_from.cancel();
                 schedule_to.cancel();
+                schedule_reset.cancel();
                 callback();
             } catch (e) {
                 callback();
@@ -128,6 +131,16 @@ function main() {
             }else night_rest = state.val;
         }
     });
+    adapter.getState('info.log', (err, state)=>{
+        if(err){
+            adapter.log.error(err);
+        }else{
+            if(state == null){
+                log_list = [];
+                adapter.setState('info.log', []);
+            }else log_list = state.val;
+        }
+    });
     if(adapter.config.circuits)split_states(adapter.config.circuits);
     else adapter.log.info('no states configured!');
     send_instances = split_arr(adapter.config.sendTo);
@@ -156,20 +169,29 @@ function enable(id, state){
     });
     if(is_alarm){
         adapter.setState('info.log', `${L.act_not} ${get_name(ids_alarm)}`);
-        if(log)adapter.log.info(`${L.act_not} ${get_name(ids_alarm)}`);
+        if(log){
+            adapter.log.info(`${L.act_not} ${get_name(ids_alarm)}`);
+            logging(`${L.act_not} ${get_name(ids_alarm)}`);
+        }
         adapter.setState('status.activation_failed', true);
         sayit(adapter.config.text_failed);
         return;
     }
     if(!adapter.config.opt_warning && is_warning){
         adapter.setState('info.log', `${L.act_not} ${get_name(ids_warning)}`);
-        if(log)adapter.log.info(`${L.act_not} ${get_name(ids_warning)}`);
+        if(log){
+            adapter.log.info(`${L.act_not} ${get_name(ids_warning)}`);
+            logging(`${L.act_not} ${get_name(ids_warning)}`);
+        }
         adapter.setState('status.activation_failed', true);
         sayit(adapter.config.text_failed);
         return;
     }
     adapter.setState('info.log', `${L.act}`);
-    if(log)adapter.log.info(`${L.act}`);
+    if(log){
+        adapter.log.info(`${L.act}`);
+        logging(`${L.act}`);
+    }
     sayit(adapter.config.text_activated);
     adapter.setState('status.activated', true);
     adapter.setState('status.deactivated', false);
@@ -177,7 +199,10 @@ function enable(id, state){
     if(warning.includes(id)){
         adapter.setState('status.activated_with_warnings', true);
         adapter.setState('info.log', `${L.act_warn} ${get_name(id)}`);
-        if(log)adapter.log.info(`${L.act_warn} ${get_name(id)}`);
+        if(log){
+            adapter.log.info(`${L.act_warn} ${get_name(id)}`);
+            logging(`${L.act_warn} ${get_name(id)}`);
+        }
         if(warning_message) messages(`${L.act_warn} ${get_name(id)}`);
     }
 }
@@ -189,7 +214,10 @@ function disable(){
     if(activated){
         adapter.setState('info.log', `${L.deact}`);
         sayit(adapter.config.text_deactivated);
-        if(log)adapter.log.info(`${L.deact}`);
+        if(log){
+            adapter.log.info(`${L.deact}`);
+            logging(`${L.deact}`);
+        }
         adapter.setState('status.siren', false);
         adapter.setState('status.activated', false);
         adapter.setState('status.deactivated', true);
@@ -294,10 +322,16 @@ function change(id, state){
             return;
         }
     }
+    else if(id === adapter.namespace + '.info.log'){
+        log_list = state.val;
+    }
     if(alarm.includes(id) && activated && isTrue(id, state)){
         adapter.setState('info.log', `${L.burgle} ${get_name(id)}`);
         sayit(adapter.config.text_alarm);
-        if(log)adapter.log.info(`${L.burgle} ${get_name(id)}`);
+        if(log){
+            adapter.log.info(`${L.burgle} ${get_name(id)}`);
+            logging(`${L.burgle} ${get_name(id)}`);
+        }
         if(alarm_message) messages(`${L.burgle} ${get_name(id)}`);
         adapter.setState('status.burglar_alarm', true);
         adapter.setState('status.siren', true);
@@ -308,13 +342,19 @@ function change(id, state){
     }
     if(warning.includes(id) && activated && isTrue(id, state)){
         adapter.setState('info.log', `${L.warn} ${get_name(id)}`);
-        if(log)adapter.log.info(`${L.warn} ${get_name(id)}`);
+        if(log){
+            adapter.log.info(`${L.warn} ${get_name(id)}`);
+            logging(`${L.warn} ${get_name(id)}`);
+        }
         if(warning_message) messages(`${L.warn} ${get_name(id)}`);
         return;
     }
     if(night.includes(id) && night_rest && isTrue(id, state)){
         adapter.setState('info.log', `${L.night} ${get_name(id)}`);
-        if(log)adapter.log.info(`${L.night} ${get_name(id)}`);
+        if(log){
+            adapter.log.info(`${L.night} ${get_name(id)}`);
+            logging(`${L.night} ${get_name(id)}`);
+        }
         if(night_message) messages(`${L.night} ${get_name(id)}`);
         return;
     }
@@ -334,6 +374,7 @@ function set_subs(){
     });
     adapter.subscribeStates('use.*');
     adapter.subscribeStates('status.*');
+    adapter.subscribeStates('info.log');
 }
 //##############################################################################
 
@@ -521,11 +562,24 @@ function shortcuts(id, val){
     }
 }
 
+function timeStamp(){
+    const date = new Date();
+    return ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2);
+}
+
+function logging(content){
+    log_list.push(timeStamp() + ' ' + content);
+    adapter.setState('info.log', log_list);
+}
+
 //##############################################################################
 
 //################# SCHEDULES ####################################################
 
 function set_schedules(){
+    schedule_reset = schedule.scheduleJob({hour: 00, minute: 00}, ()=>{
+        adapter.setState('info.log', []);
+    });
     if(adapter.config.night_from && adapter.config.night_to){
         let from, to;
         try{
@@ -537,19 +591,28 @@ function set_schedules(){
         }
         schedule_from = schedule.scheduleJob({hour: parseInt(from[0]), minute: parseInt(from[1])}, ()=>{
             adapter.setState('info.log', `${L.sleep_b}`);
-            if(log)adapter.log.info(`${L.sleep_b}`);
+            if(log){
+                adapter.log.info(`${L.sleep_b}`);
+                logging(`${L.sleep_b}`);
+            }
             adapter.setState('status.sleep', true);
             check(night, (val, ids)=>{
                 if(val){
                     if(night_message) messages(`${L.nights_b_w} ${get_name(ids)}`);
                     adapter.setState('info.log', `${L.nights_b_w} ${get_name(ids)}`);
-                    if(log)adapter.log.info(`${L.nights_b_w} ${get_name(ids)}`);
+                    if(log){
+                        adapter.log.info(`${L.nights_b_w} ${get_name(ids)}`);
+                        logging(`${L.nights_b_w} ${get_name(ids)}`);
+                    }
                 }
             });
         });
         schedule_to = schedule.scheduleJob({hour: parseInt(to[0]), minute: parseInt(to[1])}, ()=>{
             adapter.setState('info.log', `${L.sleep_e}`);
-            if(log)adapter.log.info(`${L.sleep_e}`);
+            if(log){
+                adapter.log.info(`${L.sleep_e}`);
+                logging(`${L.sleep_e}`);
+            }
             adapter.setState('status.sleep', false);
         });
     }else{
